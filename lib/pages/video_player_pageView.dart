675a7f72c047896b8a943e15608ab981b821f61e
kawezitoOBrabo <ksf4@aluno.ifal.edu.br>
2025-02-10 17:16:59 -0300

    Adição do FutureBuilder sem a API

--- lib/pages/video_player_pageView.dart
+++ lib/pages/video_player_pageView.dart
@@ -11,45 +11,55 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
-  List<DomainVideoPlayer> _videos = [];
-  bool _isLoading = true;
+  late Future<List<DomainVideoPlayer>> futureVideos;
 
   @override
   void initState() {
     super.initState();
-    _loadVideos();
+    futureVideos = _loadVideos();
   }
 
-  Future<void> _loadVideos() async {
+  Future<List<DomainVideoPlayer>> _loadVideos() async {
     try {
       PaginaDao dao = PaginaDao();
-      List<DomainVideoPlayer> videos = await dao.listaPaginas();
-      setState(() {
-        _videos = videos;
-        _isLoading = false;
-      });
+      return await dao.listaPaginas();
     } catch (e) {
       print("Erro ao carregar os vídeos: $e");
-      setState(() {
-        _isLoading = false;
-      });
+      return [];
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : PageView.builder(
-        scrollDirection: Axis.vertical,
-        pageSnapping: true,
-        itemCount: _videos.length,
-        itemBuilder: (context, index) {
-          return VideoPlayerScreen(
-            domain_videoPlayer: _videos[index],
-            telaIdAtual: index + 1,
-          );
+      body: FutureBuilder<List<DomainVideoPlayer>>(
+        future: futureVideos,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Ocorreu um erro inesperado!'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData) {
+            List<DomainVideoPlayer> videos = snapshot.data!;
+            return PageView.builder(
+              scrollDirection: Axis.vertical,
+              pageSnapping: true,
+              itemCount: videos.length,
+              itemBuilder: (context, index) {
+                return VideoPlayerScreen(
+                  domain_videoPlayer: videos[index],
+                  telaIdAtual: index + 1,
+                );
+              },
+            );
+          } else {
+            // Caso não haja dados
+            return const Center(child: Text('Nenhum vídeo encontrado.'));
+          }
         },
       ),
     );
